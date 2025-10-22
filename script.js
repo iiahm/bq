467ef584ca4b8ff030d590b008c6e22d3830a391
iiahm <ahmadabdelrazzq@gmail.com>
2025-10-22 18:19:42 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-  const password = document.getElementById("admin-password").value;
-  if (password === "mangaQadmin") {
-    document.getElementById("admin-panel").style.display = "block";
-    document.getElementById("login").style.display = "none";
-  } else {
-    document.getElementById("error-msg").textContent = "كلمة المرور خاطئة!";
-  }
-}
